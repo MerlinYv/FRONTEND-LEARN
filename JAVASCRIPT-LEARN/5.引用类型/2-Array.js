cdf757cafbef2e201cf1f40f25b8b492990bb194
shanyu <841831069@qq.com>
2021-03-25 13:22:07 +0800

    second commit

--- JAVASCRIPT-LEARN/5.引用类型/2-Array.js
+++ JAVASCRIPT-LEARN/5.引用类型/2-Array.js
@@ -20,8 +20,31 @@ console.log(arr.valueOf());
 
 /*
  * 栈方法
- * 1.push() 接受任意数量参数，返回新数组的长度
+ * 1.push() 接受任意数量参数，添加在数组尾部，返回新数组的长度
  * 2.pop() 弹出数组最后一项，返回被弹出的数组项的值
  */
 console.log(arr.push('black', 'green'));
 console.log(arr.pop());
+
+/*
+ * 队列方法
+ * 1.unshift() 接受任意数量参数，添加在数组头部，返回新数组的长度
+ * 2.shift() 弹出数组第一项，返回被弹出的数组项的值
+ */
+console.log(arr.unshift('orange'));
+console.log(arr.shift());
+
+/*
+ * 重排序方法
+ * 1.reverse() 
+ * 2.sort() 接受一个比较函数来规定排序规则
+ */
+console.log(arr.reverse());
+console.log(arr.sort());
+
+/*
+ * 操作方法
+ * 1.concat()
+ * 2.slice()
+ * 3.splice() 
+ */
